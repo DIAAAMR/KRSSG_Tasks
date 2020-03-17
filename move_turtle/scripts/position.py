e9ef4ddac67017554df7514f14ad4de37bd4688c
DIAAAMR <62210125+DIAAAMR@users.noreply.github.com>
2020-03-17 05:33:21 +0530

    Update position.py

--- move_turtle/scripts/position.py
+++ move_turtle/scripts/position.py
@@ -3,25 +3,31 @@ import rospy
 from turtlesim.msg import Pose
 from move_turtle.srv import *
 
-position=Pose
+#saves current position published to /Pose
+position=Pose()
 
+#called when request is made by another node to provide current point
 def sendPosition(requested):
 	print(requested)
+	#if request is true 
 	if requested.c==1:
 		print(position)
-		x=position.x
-		y=position.y
-		theta=position.theta
+		#current position is sent, the datatype is the the response type of newPosition and it expects all defined arguments accurately
 		return newPositionResponse(position.x, position.y, position.theta, position.linear_velocity, position.angular_velocity)
 
+#called when data is received from /Pose
 def collect(data):
 	global position
+	#updates current position 
 	position = data
 
 def main():
 	global position
+	#node is initialised by roscore
 	rospy.init_node('position', anonymous=True)	
+	#subscribes to topic /Pose and collects latest position
 	rospy.Subscriber("/turtle1/pose", Pose, collect)
+	#starts a service which sends current position on response by invoking sendPosition function
 	s = rospy.Service('positionService', newPosition, sendPosition)
 	rospy.spin()
 
